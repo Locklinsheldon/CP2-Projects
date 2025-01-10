52296a733a33b854e71b13cbc5428f459d116960
Locklinsheldon <locklin.sheldon@ucas-edu.net>
2025-01-10 13:17:24 -0700

    gitgone

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -2,6 +2,18 @@
 
 #this is the financial calculator program
 
-def menu():
-    print("Wh")
+
+
+def main():
     
+    goal = input("What is you goal to save for by the end of the year?: ")
+    Rizz()
+
+def Rizz():
+    mond = input("How much do you earn monthly?: ")
+    aloeneeds = mond*5/10
+    aloewants = mond*3/10
+    aloesave = mond*2/10
+    print("You should spend", aloeneeds, "on needs,", aloewants, "on wants, and you should deposit", aloesave, "into your savings account.")
+
+main()
