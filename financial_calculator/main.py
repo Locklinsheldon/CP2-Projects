be54771c64c4adfc8adc51da4c4c1bd2be8c6157
Locklinsheldon <Locklin.sheldon@ucas-edu.net>
2025-01-13 23:30:00 -0700

    Update main.py

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -2,16 +2,16 @@
 
 # This is the financial calculator program
 
-def main():
+def main(): #Main menu for selecting options
     while True:
-        print("\nFinancial Calculator:\n")
+        print("\nFinancial Calculator:\n") #Title
         print("1. Save for a Goal")
         print("2. Interest Calculator")
         print("3. Budget Allocator (Recommended to do first)")
         print("4. Sale Price Calculator")
         print("5. Tip Calculator")
         
-        choice = input("\nPick (1-5): ")
+        choice = input("\nPick (1-5): ")#User choice
         
         if choice == '1':
             SFG()
@@ -24,43 +24,41 @@ def main():
         elif choice == '5':
             TC()
         else:
-            print("Invalid input. Try again.")
+            print("Bro duznt no how too tipe. Retri, sily guse.")#Tells the user if they mispelled or put in invalid characters
 
-def SFG():
+def SFG(): #Save for goal function
     goal = float(input("Your monthly savings goal: "))
     deposit = float(input("Amount you save each week: "))
     
     timeUntil = goal / deposit
-    print("It'll take you", timeUntil, "weeks to save for your goal.")
+    print("It'll take you", timeUntil, "weeks to save for your goal.")#Tells user when they will have reached their goal
 
-def CIC():
+def CIC(): #Compound interest calculator function
     principal = float(input("Enter the principal amount: "))
     rate = float(input("Annual interest rate (in %): ")) / 100
     timesCompounded = int(input("Number of times te interest is compounded per year: "))
     years = int(input("Number of years: "))
     
-    amount = principal * (1 + rate / timesCompounded) ** (timesCompounded * years)
-    print("The amount after", years, "years will be: $", amount)
+    amount = principal * (1 + rate / timesCompounded) ** (timesCompounded * years)#Algorithm for finding compound interest
+    print("The amount after", years, "years will be: $", amount) 
 
-def BA():
+def BA(): #Budget allocator function
     mond = float(input("How much do you earn weekly: "))
     aloeneeds = mond * 0.50
     aloewants = mond * 0.30
     aloesave = mond * 0.20
-    print("You should spend $", aloeneeds, "on needs, $", aloewants, "on wants, and you should deposit $", aloesave, "into your savings account.")
+    print("You should spend $", aloeneeds, "on needs, $", aloewants, "on wants, and you should deposit $", aloesave, "into your savings account.") #Tells the user what they should spend their money on
 
-def SPC():
-    ogPrice = float(input("Enter the original price: $"))
-    disPerc = float(input("Enter the discount percentage (without percent sign): ")) / 100
+def SPC(): #Sale price calculator function
+    ogPrice = float(input("Original price: $"))
+    disPerc = float(input("Discount percentage (without percent sign): ")) / 100
     salePrice = ogPrice * (1 - disPerc)
-    print("The sale price is: $", salePrice)
+    print("The sale price is: $", salePrice)#Tells the user the new sale price after discounts
 
-def TC():
+def TC(): #Tip calculator function
     billAm = float(input("Enter the bill amount: $"))
-    tipPerc = float(input("Enter the tip percentage (without percent sign): ")) / 100
+    tipPerc = float(input("Enter the tip percentage (without percent sign): ")) / 100 #Finds tip percentage
     tipAm = billAm * tipPerc
-    totAm = billAm + tipAm
-    print("The tip amount is: $", tipAm)
-    print("The total amount to be paid is: $", totAm)
+    print("The tip amount is: $", tipAm)#Tells the user how much the tip is
 
 main()
