266803203eea23283ec51ed34bc0d617ff6c3f19
Locklinsheldon <Locklin.sheldon@ucas-edu.net>
2025-02-13 13:22:42 -0700

    Update RPG.py

--- RPG.py
+++ RPG.py
@@ -1,12 +1,24 @@
+import random
+
+lowlet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+(random.choice(lowlet))
+
+uplet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
 def upnlow():
-    print("erm")
+    lowlet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    (random.choice(lowlet))
 def numberz():
     print("erm")
 def speshal():
     print("erm")
 def main():
+    pass1 = []
+    pass2 = []
+    pass3 = []
+    pass4 = []
     print("Welcome to the Random Password Generator.")
-    length = input("How long does your password need to be?: ")
+    length = input("How long do you want your password to be or how long does your password need to be?: ")
     uplow = input("Do you need upper and lowercase letters?(y/n): ")
     if uplow == "y":
         upnlow()
