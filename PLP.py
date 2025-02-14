17e3b57a0d30f2cffb8a27ab96f7b032c5bf9bae
Locklinsheldon <Locklin.sheldon@ucas-edu.net>
2025-02-14 13:41:56 -0700

    Update PLP.py

--- PLP.py
+++ PLP.py
@@ -1,23 +1,44 @@
-
-libr = []
-
+libr = set()
 
 def addnew():
-    print()
+    new = input("What would you like to add to your personal library?: ")
+    if new in libr:
+        print("Sorry, this item is already in your library.")
+    else:
+        libr.add(new)
+        print("Updated library:", libr)
+
+
 def search():
     item = input("What is the EXACT name of the item you're looking for?: ")
     if item in libr:
         searem = input("This item is in your list. Would you like to remove this item?(y/n)")
         if searem == "y":
-            libr - item
-        if searem == "n":
-            print("This item will stay in your list.")
+            libr.remove(item)
+        elif searem == "n":
+            print("This item will stay in your library.")
+        else:
+            print("Sorry, that isn't an option :(")
+
 def remove():
     rem = input("What item would you like to be removed?(type it's EXACT name): ")
-    libr - rem
+    if rem in libr:
+        libr.remove(rem)
+        print("Updated library:", libr)
+    else:
+        print("This item may not be in your library, or perhaps the name was not typed properly.")
+
+def view():
+    print("Current library:"libr)#Continue from here
+
 def ex():
-    while True:
-        print("GOODBYE")
+    proc = input("Are you sure you want to end the program? All items in your library will be lost(y/n): ")
+    if proc == "y":
+        while True:
+            print("GOODBYE")
+    else:
+        print("The program will continue.")
+
 def main():
     while True:
         print("Personal Library Program")
@@ -25,19 +46,17 @@ def main():
         print("2. Search for a specific item in your library")
         print("3. Remove an item from your personal library")
         print("4. Exit the program")
-       
+        
         action = input("What would you like to do?: ")
-
+        if action == "1":
+            addnew()
 
         if action == "2":
             search()
 
-
         if action == "3":
             remove()
-
 
         if action == "4":
             ex()
 main()
-
