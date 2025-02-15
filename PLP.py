1470bb8ea3c942f2021ac6ad9a79135c770243f0
Locklinsheldon <Locklin.sheldon@ucas-edu.net>
2025-02-14 17:32:29 -0700

    Update PLP.py

--- PLP.py
+++ PLP.py
@@ -1,62 +1,71 @@
-libr = set()
+libr = set()#Library
 
-def addnew():
-    new = input("What would you like to add to your personal library?: ")
-    if new in libr:
-        print("Sorry, this item is already in your library.")
+def addNew():#Function for adding new items
+    newItem = input("\nWhat would you like to add to your personal library?: ")#Asks what item
+    if newItem in libr:#Checks if item is in library
+        print("\nSorry, this item is already in your library.")
     else:
-        libr.add(new)
-        print("Updated library:", libr)
+        libr.add(newItem)#Adds item to library
+        print("\nUpdated library:", libr)#Prints updated library
 
 
-def search():
-    item = input("What is the EXACT name of the item you're looking for?: ")
-    if item in libr:
-        searem = input("This item is in your list. Would you like to remove this item?(y/n)")
-        if searem == "y":
-            libr.remove(item)
-        elif searem == "n":
-            print("This item will stay in your library.")
+def search():#Function for checking if an item is in the library
+    searItem = input("\nWhat is the EXACT name of the item you're looking for?: ")#Asks for the name of the item
+    if searItem in libr:#Checks if the item is in the library
+        seaRem = input("\nThis item is in your list. Would you like to remove this item?(y/n): ")#Asks if they would like the item to be removed
+        if seaRem == "y":
+            libr.remove(searItem)#Removes the item
+            print("\nUpdated library:", libr)
+        elif seaRem == "n":
+            print("\nThis item will stay in your library.")
+            print("\nLibrary:", libr)
         else:
-            print("Sorry, that isn't an option :(")
+            print("\nSorry, that isn't an option :(")#Error message
 
-def remove():
-    rem = input("What item would you like to be removed?(type it's EXACT name): ")
-    if rem in libr:
-        libr.remove(rem)
-        print("Updated library:", libr)
+def remove():#Function for removing an item
+    remItem = input("\nWhat item would you like to be removed?(type it's EXACT name): ")#Asks what item
+    if remItem in libr:#Checks if the item is in the library
+        libr.remove(remItem)
+        print("\nUpdated library:", libr)
     else:
-        print("This item may not be in your library, or perhaps the name was not typed properly.")
+        print("\nThis item may not be in your library, or perhaps the name was not typed properly.")#Error message for user
 
-def view():
-    print("Current library:"libr)#Continue from here
+def view():#Function for viewing the current library
+    print("\nCurrent library:", libr)
 
-def ex():
-    proc = input("Are you sure you want to end the program? All items in your library will be lost(y/n): ")
-    if proc == "y":
+def ex():#Function for ending the programm
+    leave = input("\n!ALL ITEMS IN YOUR LIBRARY WILL BE LOST!. Are you sure you want to end the program?(y/n): ")#Warning message for user
+    if leave == "y":
         while True:
-            print("GOODBYE")
+            print("\nG O O D B Y E")#Prints goodbye indefinetley
     else:
-        print("The program will continue.")
+        print("\nThe program will continue.")
 
-def main():
-    while True:
-        print("Personal Library Program")
-        print("1. Add a new item to your personal library")
+def main():#Menu for the user to select what they would like to do
+    print("\nPersonal Library Program")    
+    while True:#Makes the menu recur
+        print("\n1. Add a new item to your personal library")
         print("2. Search for a specific item in your library")
         print("3. Remove an item from your personal library")
-        print("4. Exit the program")
+        print("4. View current library")
+        print("5. Exit the program")
         
-        action = input("What would you like to do?: ")
+        action = input("\nWhat would you like to do?: ")
         if action == "1":
-            addnew()
+            addNew()
 
-        if action == "2":
+        elif action == "2":
             search()
 
-        if action == "3":
+        elif action == "3":
             remove()
 
-        if action == "4":
+        elif action == "4":
+            view()
+
+        elif action == "5":
             ex()
-main()
+
+        else:
+            print("\nThat is not an available action.")#Error message
+main()#Starts the program
