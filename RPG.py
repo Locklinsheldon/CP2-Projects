e064866309b1deadeb54f280594fa5f24bb6645f
Locklinsheldon <Locklin.sheldon@ucas-edu.net>
2025-02-20 14:36:42 -0700

    Update RPG.py

--- RPG.py
+++ RPG.py
@@ -1,37 +1,108 @@
 import random
 
-lowlet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-(random.choice(lowlet))
+print("\nWelcome to the Random Password Generator")
 
-uplet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+length = input("\nHow long does your password need to be?(1-100): ")
 
-def upnlow():
-    lowlet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    (random.choice(lowlet))
-def numberz():
-    print("erm")
-def speshal():
-    print("erm")
+askUp = input("\nDo you need uppercase letters in your password?(y/n): ")
+if askUp == "y":
+    upTrue = True
+else:
+    upTrue = False
+
+askSpec = input("\nDo you need special characters in your password?(y/n): ")
+if askSpec == "y":
+    specTrue = True
+else:
+    specTrue = False
+
+askNum = input("\nDo you need numbers in your password?(y/n): ")
+if askNum == "y":
+    numTrue = True
+else:
+    numTrue = False
+
+if upTrue == True and specTrue == True and numTrue == True:
+    allTrue = True
+if upTrue == True and specTrue == True and numTrue != True:
+        threeTrue = True
+if upTrue == True and specTrue != True and numTrue != True:
+    twoTrue = True
+if upTrue != True and specTrue != True and numTrue != True:
+    oneTrue = True
+
+runCount = (0)
+
 def main():
-    pass1 = []
-    pass2 = []
-    pass3 = []
-    pass4 = []
-    print("Welcome to the Random Password Generator.")
-    length = input("How long do you want your password to be or how long does your password need to be?: ")
-    uplow = input("Do you need upper and lowercase letters?(y/n): ")
-    if uplow == "y":
-        upnlow()
-    elif uplow == "n":
-        print("There will be no uppercase letters in your password.")
-    nums = input("Does your password need numbers?(y/n): ")
-    if nums == "y":
-        numberz()
-    elif nums == "n":
-        print("There will be no numbers in your password.")
-    spechar = input("Does your password need special characters?(y/n): ")
-    if spechar == "y":
-        speshal()
-    elif spechar == "n":
-        print("There will be no special characters in your password.")
-main()
+    ranChar()   
+
+   
+password1 = []
+password2 = []
+password3 = []
+password4 = []
+
+
+
+def ranChar():
+    lowLet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    upLet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    specLet = ["!", "@", "#", "$", "%", "&"]
+    nummerz = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]    
+    
+    
+    if allTrue == True:
+        char4one = (random.choice(lowLet))
+        char4two = (random.choice(upLet))
+        char4three = (random.choice(specLet))
+        char4four = (random.choice(nummerz))
+        pickList4 = (char4one, char4two, char4three, char4four)
+        ranPick4 = (random.choice(pickList4))
+    
+    elif threeTrue == True:
+        char3one = (random.choice(lowLet))
+        char3two = (random.choice(upLet))
+        char3three = (random.choice(specLet))
+        pickList3 = (char3one, char3two, char3three)
+        ranPick3 = (random.choice(pickList3))
+    
+    elif twoTrue == True:
+        char2one = (random.choice(lowLet))
+        char2two = (random.choice(upLet))
+        pickList2 = (char2one, char2two)
+        ranPick2 = (random.choice(pickList2))
+    
+    elif oneTrue == True:
+        char1one = (random.choice(lowLet))
+        pickList1 = (char1one)
+        ranPick1 = (random.choice(pickList1))
+
+    if allTrue == True:
+        if runCount < length:
+            password1.append(ranPick4)
+            runCount + 1
+        else:
+            pass
+
+    if threeTrue == True:
+        if runCount < length:
+            password1.append(ranPick3)
+            runCount + 1
+        else:
+            pass
+
+    if twoTrue == True:
+        if runCount < length:
+            password1.append(ranPick2)
+            runCount + 1
+        else:
+            pass
+    
+    if oneTrue == True:
+        if runCount < length:
+            password1.append(ranPick1)
+            runCount + 1
+        else:
+            pass
+
+ranChar()
