81338526a8f64698f8ba386c9f53889e11e30b96
Locklinsheldon <Locklin.sheldon@ucas-edu.net>
2025-04-01 20:41:20 +0000

    Bru

--- PLP/rething.py
+++ PLP/rething.py
@@ -2,20 +2,14 @@ libr = []
 
 t = True
 
-
 def addItem():
     name = input("What is the name of your item?: ")
-
     auth = input("Who is the author of this item?: ")
-
     rDate = input("When was this item first released?: ")
-
     genre = input("What is this item's genre?: ")
-
-
     info = {'Item': len(libr), 'Name': name, 'Author': auth, 'Release Date': rDate, 'Genre': genre}
+    
     libr.append(info)
-
     print(libr)
 
 def remove():
@@ -23,9 +17,22 @@ def remove():
     libr.pop(which)
 
 def change():
-    "real"
+    askIt = input("Which item are you looking for?(0-999): ")
+    if askIt in libr:
+        print(libr(askIt))
+        print("1. Name\n 2. Author\n 3. Release Date\n 4. Genre")
+        askDet = input("What detail do you want to change?(1-4): ")
+        askWhat = input("What would you like to change it to?: ")
+        if askDet == "1":
+            libr.update({name: askWhat})
+        if askDet == "2":
+            libr.update({auth})
 
 def menu():
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. Update Item")
+    print("4. View Library")
     ask = input("What would you like to do?: ")
     if ask == "1":
         addItem()
@@ -33,7 +40,6 @@ def menu():
         remove()
     elif ask == "3":
         change()
-
     elif ask == "4":
         print(libr)
 
