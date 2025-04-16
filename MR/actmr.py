2cff2b3aaacc780e19a0f8835ffd64faeba23828
Locklinsheldon <Locklin.sheldon@ucas-edu.net>
2025-04-16 20:36:09 +0000

    l

--- MR/actmr.py
+++ MR/actmr.py
@@ -1,71 +1,57 @@
-#Imports csv
-import csv
 
-#Welcomes user
-print("\nWelcome to the Movie Recommender")
+import csv#Imports csv
 
-#Function for showing the names of all of the movies
-def show_movies(path='MR/movies.csv'):
+print("\nWelcome to the Movie Recommender")#Welcomes user
+
+def show_movies(path='MR/movies.csv'):#Function for showing the names of all of the movies
     try:
-        #Opens the csv file and prints it
-        with open(path, mode='r') as file:
+        with open(path, mode='r') as file:#Opens the csv file and prints it
             reader = csv.reader(file)
             for row in reader:
                 print(row)
-                #Error for if the file can't be found
-    except FileNotFoundError:
+    except FileNotFoundError:#Error for if the file can't be found
         print("<Couldn't find movie>")
 
-#Function for the user to pick which genre
-def genre():
-    #List of all genres
-    genre = ["Drama", "Comedy", "Sci-Fi", "Adventure", "Family", "Animation", "History", "Action", "Fantasy", "Biography", "Sport", "Musical", "Romance", "War", "Crime", "Mystery", "Thriller", "Music"]
+def genre():#Function for the user to pick which genre
+    genre = ["Drama", "Comedy", "Sci-Fi", "Adventure", "Family", "Animation", "History", "Action", "Fantasy", "Biography", "Sport", "Musical", "Romance", "War", "Crime", "Mystery", "Thriller", "Music"]#List of all genres
     print("\nAvailable Genres:")
-    #Makes the users answer work
-    for index, x in enumerate(genre, start=1):
+    for index, x in enumerate(genre, start=1):#Makes the users answer work
         print(f"{index}. {x}")
     
-    #Makes happen while true
-    while True:
+    while True:#Makes happen while true
         try:
-            #User choice is taken
-            choice = int(input("\nWhat genre?(1-18): "))
+            choice = int(input("\nWhat genre?(1-18): "))#User choice is taken
             if 1 <= choice <= 18:
-                #Tells the user what genre they selected
-                print(f"\nGenre selected: {genre[choice - 1]}")
-                #Takes the user back to the menu
-                break
+                print(f"\nGenre selected: {genre[choice - 1]}")#Tells the user what genre they selected
+                break#Takes the user back to the menu
             else:
-                #Error message
-                print("<Input needs to be a number(1-18)>")
+                print("<Input needs to be a number(1-18)>")#Error message
         except ValueError:
             print("<Input needs to be a number(1-18)>")
 
-#Function for user to select which director
-def direct():
-    #Users choice of director
-    dire = input("\nEnter the director's name: ")
+def direct():#Function for user to select which director
+    dire = input("\nEnter the director's name: ")#Users choice of director
     print(f"\nSearching for movies directed by {dire}...")
 
-def length():
+def length():#Function for getting how long the user wants the movie to be
     try:
-        min_length = int(input("\nEnter minimum movie length in minutes: "))
-        max_length = int(input("\nEnter maximum movie length in minutes: "))
+        min_length = int(input("\nEnter minimum movie length in minutes: "))#Variable for the minimum length
+        max_length = int(input("\nEnter maximum movie length in minutes: "))#Variable for the maximum length
         print(f"\nSearching for movies between {min_length}-{max_length} minutes...")
-    except ValueError:
+    except ValueError:#Error for if its not a number
         print("<Input needs to be a number>")
 
-def actors():
-    act = input("\nEnter an actor's name: ")
+def actors():#Function for finding which actors the use wants
+    act = input("\nEnter an actor's name: ")#Variable for which actor
     print(f"\nSearching for movies with {act}...")
 
-def print_list():
-    with open('MR/movies.csv', new_line='') as csv_file:
+def print_list():#Function for printing all of the movies
+    with open('MR/movies.csv', new_line='') as csv_file:#Opens, then closes the movies csv
         csv_reader = csv.reader(csv_file)
         for row in csv_reader:
             print(row)
 
-def find():
+def find():#Function for finding which movie
     print("\nSearch Options:")
     print("1. Genre\n2. Directors\n3. Length\n4. Actors")
     
@@ -91,7 +77,7 @@ def find():
         else:
             print("<Input needs to be a number(1-4)")
 
-def menu():
+def menu():#Menu for selecting what you want to do
     print("\n1. Print All Movies\n2. Find A Movie\n3. End Program")
     which = input("What would you like to do?(1-3): ")
     if which == "1":
@@ -105,5 +91,5 @@ def menu():
     else:
         print("<Input needs to be a number(1-3)>")
 
-while True:
+while True:#Runs the program while true
     menu()
