a18cb47f885bf6cca835cf8704c00f1b1de1518d
Locklinsheldon <Locklin.sheldon@ucas-edu.net>
2025-04-30 20:53:44 +0000

    luzduit

--- battle_sim/battle_sim.py
+++ battle_sim/battle_sim.py
@@ -1,5 +1,10 @@
 import pandas as pd
+print("Welcome to the Ultimate Battle Simulator")
 
-df = pd.read_csv('char.csv')
+def tutorial():
+    print("\nTo start, the first thing you will need to do is create your character.")
+    print("\nWhat would you like this character's name to be?")
+    input("Name: ")
 
+df = pd.read_csv('battle_sim/char.csv')
 print(df.to_string())
