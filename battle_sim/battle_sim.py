8d4660063e972898cac79fbe85db32aa46083b8b
Locklinsheldon <Locklin.sheldon@ucas-edu.net>
2025-05-01 18:09:36 +0000

    plerm

--- battle_sim/battle_sim.py
+++ battle_sim/battle_sim.py
@@ -4,7 +4,13 @@ print("Welcome to the Ultimate Battle Simulator")
 def tutorial():
     print("\nTo start, the first thing you will need to do is create your character.")
     print("\nWhat would you like this character's name to be?")
-    input("Name: ")
+    tutor_name = input("\nName: ")
+    if tutor_name == "":
+        tutor_name = "[No name]"
+    print("Now that you have given your character a name, you must give it it's health, strength, defense, and speed atributes.")
 
-df = pd.read_csv('battle_sim/char.csv')
-print(df.to_string())
+def show_char():
+    df = pd.read_csv('battle_sim/char.csv')
+    print(df.to_string())
+
+tutorial()
